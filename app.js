f0374e53eba46a939192e172d287a30d3c90ee39
Andrew <aaoyanguren@outlook.com>
2021-02-09 11:48:50 -0800

    Completed JavaScript, and deleted toasts divs from HTML

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 // ====== Variables ===== //
+const button = document.getElementById('btn');
+const toasts = document.getElementById('toasts');
 
-
-
+const messages = [
+  'Message One',
+  'Message Two',
+  'Message Three',
+  'Message Four',
+]
 // ====== Event Listeners ===== //
 
-
+button.addEventListener('click', () => createNotifcation())
 
 // ====== Functions ===== //
+
+const createNotifcation = () => {
+  const newToast = document.createElement('div');
+  newToast.classList.add('toast');
+
+  newToast.innerText = getRandomMessage();
+
+  toasts.appendChild(newToast)
+
+  setTimeout(() => {
+    newToast.remove()
+  }, 3000)
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
